08a0a31aaad18f1e955a3f2a0e5f19693116b135
hynk_mac <hynk1240@gmail.com>
2026-06-30 03:30:17 +0900

    Add Augnes public surface route model

---
+++ app/perspective/page.tsx
@@ -0,0 +1,10 @@
+import { PerspectivePublicConstellationSurface } from "@/components/perspective/perspective-public-constellation-surface";
+
+export const metadata = {
+  title: "Perspective",
+  description: "Project constellation surface",
+};
+
+export default function PerspectivePage() {
+  return <PerspectivePublicConstellationSurface />;
+}
---
+++ app/workbench/page.tsx
@@ -0,0 +1,10 @@
+import { AugnesCockpit } from "@/components/augnes-cockpit";
+
+export const metadata = {
+  title: "Augnes Workbench",
+  description: "Augnes cockpit workbench",
+};
+
+export default function WorkbenchPage() {
+  return <AugnesCockpit />;
+}
